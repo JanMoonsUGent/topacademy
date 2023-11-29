52592eabbc14bb8c68ad5c378617002f42a6c493
JuliaKohi <147695293+JuliaKohi@users.noreply.github.com>
2023-11-29 10:21:40 +0100

    new block

---
+++ components/specificComponents/Destination/Destination.js
@@ -0,0 +1,35 @@
+﻿import React, { Component } from "react";
+import css from "./Destination.module.scss";
+import Headermenu from "../../genericComponents/Headermenu/Headermenu";
+import Hero from "../../genericComponents/Hero/Hero";
+import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
+
+export default class Destination extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div {...storyblokEditable(this.props.blok)}>
+				<Headermenu blok={this.props.menu.content}></Headermenu>
+
+				<main>
+					<Hero blok={this.props.blok} contentTypeTag="course" />
+					<div className={css["destination-page__main-content"]}>
+						<div id="destination-page__short-description" key="destination-page__short-description" className={css["destination-page__short-description"]}>
+							<section className={css["rich-text-section--with-navigator"]}>
+								<h2 className={css["rich-text-section__title"]}>Destination details</h2>
+								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
+							</section>
+						</div>
+					</div>
+
+				</main>
+			</div>
+		);
+
+	}
+}
